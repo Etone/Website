0215de23232814b667383189b68ca1525cebce58
Corvin Schapoehler <co.schapoehler@gmail.com>
2017-10-04 15:22:18 +0200

    Initialize Router for Blog
    Added Blog Display Dummy Component to initialize the Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { MainmenuComponent } from './components/mainmenu/mainmenu.component';
+import { BlogDisplayComponent } from './components/blog-display/blog-display.component';
+
+const routes: Routes = [
+  { path: "blog", component: BlogDisplayComponent },
+  { path: "**", component: BlogDisplayComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainmenuComponent
+    MainmenuComponent,
+    BlogDisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
